9675e176d7e657c8043a5dfde5bca14d2350fa02
zeynepdundarr <idundar17@ku.edu.tr>
2022-08-28 23:55:37 +0300

    restructured ConfusionMatrixManager

--- DataSourceManager.py
+++ DataSourceManager.py
@@ -10,67 +10,84 @@ class ConfusionMatrixManager:
 
     given_label_list = None
     prob_values = None
+    avg_probs_a = None
+    avg_probs_b = None
+    pred_label_list = None
 
     def __init__(self, table, model_no, labels):
-        table = table
-        model_no = model_no
-        labels = labels
+        self.table = table
+        self.model_no = model_no
+        self.labels = labels
+        print("Model initialized!")
 
-    def table_divide_and_format(self, table):
+    def table_divide_and_format(self):
         # TODO: can it be more optimized that row by row np.array conversion
         # info: divides table into actual labels list and probility values
-        self.given_label_list = [row[1] for row in table]
-        self.prob_values = [np.array(list(map(float,row[2:len(row)])), dtype = np.float32) for row in table]
+        self.given_label_list = np.array([row[1] for row in self.table])
+        print("TEST 3 - cmm.given: ", cmm.given_label_list)
+        self.prob_values = [np.array(list(map(float,row[2:len(row)])), dtype = np.float32) for row in self.table]
 
 
     def get_avg_preds(self):
         # TODO: make it scalable for n models
-
-        np_table = self.table_divide_and_format(sample_table)
+        np_table = np.array(self.prob_values)
 
         w1, w2, w3 = 0.5, 0.6, 0.7
         weights_label_a = np.array([w1, 0, w2, 0, w3, 0])
         weights_label_b = np.array([0, w1, 0, w2, 0, w3])
 
-        probs_a = np.sum(np_table*weights_label_a, axis=1)/3
-        probs_b = np.sum(np_table*weights_label_b, axis=1)/3
+        self.avg_probs_a = np.sum(np_table*weights_label_a, axis=1)/3
+        self.avg_probs_b = np.sum(np_table*weights_label_b, axis=1)/3
+     
+    # def confusion_matrix2(actual, pred):
 
-        # tests for calculation
-        # print("Test 0 - : ", np_table*weights_label_a)
-        # print("Test 1 - : ",  probs_a)
-        # print("Pred res: ", get_pred_list(probs_a, probs_b))
-        return get_pred_list(probs_a, probs_b)
+    # labels = ["A", "B"]
+    # print("actual: ", actual)
+    # print("pred: ", pred)
 
-    def confusion_matrix(self, preds):
+    # actual = np.array(actual)
+    # pred = np.array(pred)
+
+    # labels = np.unique(actual)  
+    # matrix = np.zeros((len(labels), len(labels)))
+
+    # for i in range(len(labels)):
+    #     for j in range(len(labels)):
+    #         matrix[i, j] = np.sum((actual == labels[i]) & (pred == labels[j]))
+    #         print("check matrix: ", matrix[i,j])
+    # return matrix
+
+    def confusion_matrix(self):
 
         actual = self.given_label_list
-        labels = ["A", "B"]
+        preds = self.pred_label_list
+
+        labels = np.unique(actual)
         matrix = np.zeros((len(labels), len(labels)))
 
         for i in range(len(labels)):
             for j in range(len(labels)):
-                matrix[i,j] = np.sum( actual==labels(i) & preds==labels(j))
+                matrix[i,j] = np.sum( (actual==labels[i]) & (preds==labels[j]))
         return matrix
 
+    def get_pred_list(self):
+        res = self.avg_probs_a - self.avg_probs_b
+        self.pred_label_list = ["A" if x>=0 else "B" for x in res]
 
-
-
+# Run ConfusionMatrixManager
 sample_table = [('2', 'B', '0.1', '0.9', '0.2', '0.8', '0.0', '1'),
                 ('3', 'B', '0.1', '0.9', '0.2', '0.8', '0.0', '1'),
                 ('4', 'B', '0.1', '0.9', '0.2', '0.8', '0.0', '1')]
 
 cmm = ConfusionMatrixManager(sample_table, 3, ["A", "B"])
-cmm.table_divide_and_format(sample_table)
-print("TEST 0 - given_label_list: ", cmm.given_label_list)
-print("TEST 1 - prob values: ", cmm.prob_values)
-
-#preds = get_pred_list(get_avg_preds())
-
-
+cmm.table_divide_and_format()
+cmm.get_avg_preds()
+cmm.get_pred_list()
+print("TEST 4 - cmm.given: ", cmm.given_label_list)
+print("TEST 0 - Confusion Matrix: ", cmm.confusion_matrix())
 
 
 class DataSourceManager:
-
     chunk_list = []
 
     def __init__(self, filepath, db_name):
@@ -269,9 +286,6 @@ class DataSourceManager:
 
 # print("arr1-1: ", arr1)
 
-def get_pred_list(label1_pred, label2_pred):
-    res = label1_pred - label2_pred
-    return ["A" if x>=0 else "B" for x in res]
 
 def extract_columns():
     table = [
