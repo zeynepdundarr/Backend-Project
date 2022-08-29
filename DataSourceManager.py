dffce7f03c61796dcb188640b783748a7b553aeb
zeynepdundarr <idundar17@ku.edu.tr>
2022-08-30 00:01:02 +0300

    saving confusion matrix to database

--- DataSourceManager.py
+++ DataSourceManager.py
@@ -62,48 +62,51 @@ class DataSourceManager:
         #         con.close()
         #         print('SQLite Connection closed')
 
-    def populate_data_task(self):
-        print("Start populating data...")
-        self.read_from_csv_and_insert_data()
-
-    def insert_data(self, chunk):
+    def insert_data(self, chunk, table_name):
         #print("chunk: ", chunk)
         con = sqlite3.connect(self.db_name+".db")
         cur = con.cursor()
-        cur.executemany("INSERT INTO "+self.table_name+" VALUES (?, ?, ?, ?, ?, ?, ?, ?)", chunk)
+        query = "INSERT INTO "+table_name+" VALUES (?, ?, ?, ?, ?, ?, ?, ?)"
+        cur.executemany(query, chunk)
         print("FETCH - insert_data: ", cur.fetchall())
         con.commit()
         con.close()
-    
-    def insert_data2(self, chunk):
+
+    def insert_confusion_matrix(self, table_name, confusion_matrix_str):
         con = sqlite3.connect(self.db_name+".db")
         cur = con.cursor()
-        cur.executemany("INSERT INTO "+self.table_name+" VALUES (?, ?, ?, ?, ?, ?, ?, ?)", chunk)
+        cur.execute("INSERT INTO "+table_name+" (conf_mat_str) VALUES ( '"+confusion_matrix_str+"')")
         con.commit()
-        con.close()
 
-    def create_table(self, table_name):
-        self.table_name = table_name
+    def create_data_table(self, table_name):
         con = sqlite3.connect(self.db_name+".db")
         cur = con.cursor()
         cur.execute("CREATE TABLE "+table_name+"(id,given_label,model1_A,model1_B,model2_A,model2_B,model3_A,model3_B)")
-        print("FExable: ", cur.fetchone())
+        print("FETCH: ", cur.fetchone())
         con.commit()
         con.close()
 
-    def delete_data(self):
+    def create_conf_matrix_table(self, table_name):
+        con = sqlite3.connect(self.db_name+".db")
+        cur = con.cursor()
+        cur.execute("CREATE TABLE "+table_name+"(conf_mat_str)")
+        print("FETCH - confusion matrix: ", cur.fetchone())
+        con.commit()
+        con.close()
+
+    def delete_data(self, table_name):
         con = sqlite3.connect(self.db_name+".db")
         cursor = con.cursor()
-        query = "DELETE FROM "+self.table_name
+        query = "DELETE FROM "+table_name
         cursor.execute(query)
         print("DELETION check: ", cursor.rowcount)     
         con.commit()
         con.close()
         
-    def display_table(self):
+    def display_table(self,table_name):
         con = sqlite3.connect(self.db_name+".db")
         cur = con.cursor()
-        cur.execute("SELECT * FROM "+self.table_name)
+        cur.execute("SELECT * FROM "+table_name)
         print("fetch all select: ", cur.fetchall())
         con.commit()
         con.close()
@@ -118,40 +121,62 @@ class DataSourceManager:
         con.commit()
         con.close()
         return self.window_data
-        
+    
 
 ## A sample database method
 d1 = DataSourceManager("sample_data_tazi.csv", "Tazi") 
 d1.database_connection() 
-#d1.create_table("table3")
+#d1.create_data_table("table3")
 # d1.read_from_csv_and_insert_data()
-#d1.delete_data()
+# d1.delete_data()
 # d1.display_table()
 
-# transfer data to cmm
+## do it for once
+#d1.create_conf_matrix_table(table_name_2)
+
+conf_table_name = "conf_matrix4"
+def save_confusion_matrix(table_name, confusion_matrix):
+    confusion_matrix_str = ""   
+    for row in confusion_matrix:
+        for val in row:
+            confusion_matrix_str+=" "+str(val)
+
+    d1.insert_confusion_matrix(table_name, confusion_matrix_str)
+    d1.display_table(table_name)
+
+
+## transfer data to cmm
 cmm = ConfusionMatrixManager(3, ["A","B"])
 window_range = 10
 db_size = 20
 index = 0
 
+d1.delete_data(conf_table_name)
+
+table_name = "table3"
 conf_matrix_array = []
 while index<db_size-window_range:
     start_index = index
     end_index = index + window_range
     #print("index pairs: ", (start_index, end_index))
-    query = "Select * from "+d1.table_name+" where cast(id as INT) between "+str(start_index)+" and "+str(end_index)
+    query = "Select * from "+table_name+" where cast(id as INT) between "+str(start_index)+" and "+str(end_index)
     d1.window_data = d1.execute_and_get_query(query)
     cmm.table_divide_and_format(d1.window_data)
     cmm.calculate_avg_preds()
     cmm.calculate_pred_list()
     conf_matrix = cmm.calculate_confusion_matrix()
     conf_matrix_array.append(cmm.calculate_confusion_matrix())
-    print("TEST 15: ", conf_matrix)
-    # update indexes
+
+    # start confusion matrix saving
+    save_confusion_matrix(conf_table_name, conf_matrix)
+    # end confusion matrix saving
+
     index += 1
     print("TEST 16 - index: ", index)
 
-print("TEST 1: Confusion matrix array: ", conf_matrix_array)
+d1.display_table(conf_table_name)
+
+# print("TEST 1: Confusion matrix array: ", conf_matrix_array)
   
 
 
@@ -173,7 +198,7 @@ print("TEST 1: Confusion matrix array: ", conf_matrix_array)
 #d1.read_data()
 
 
-#d1.create_table(con, d1.table_name)
+#d1.create_data_table(con, table_name)
 #d1.display_table()
 #d1.get_data_as_sliding_windows()
 
